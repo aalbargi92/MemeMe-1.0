2335f6ca52f308fbc6a314efc6ca65bdf837097c
abdullah <a.albargi92@gmail.com>
2019-10-07 11:57:36 +0300

    Initial Commit

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -8,13 +8,165 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    // MARK: Outlets
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    // MARK: Vars
+    var defaultTopString = "Top"
+    var defaultBottomString = "Bottom"
+    var selectedImage: UIImage?
+    var imagePicker = UIImagePickerController()
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cameraButton.isEnabled = UIImagePickerController.isCameraDeviceAvailable(.rear)
+        
+        reset()
+        
+        imagePicker.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unsubscribeToKeyboardNotifications()
     }
 
-
+    // MARK: Actions
+    @IBAction func sharePressed(_ sender: Any) {
+        if let image = takeScreenshot() {
+            let activity = UIActivityViewController(activityItems: [image], applicationActivities: [])
+            activity.completionWithItemsHandler = {(_, completed, _, _) in
+                if completed {
+                    return
+                }
+            }
+            present(activity, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+       reset()
+    }
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        showImagePicker()
+    }
+    
+    @IBAction func albumPressed(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        showImagePicker()
+    }
+    
+    // MARK: Helpers
+    private func reset() {
+        selectedImage = nil
+        imageView.image = nil
+               configureTextField(topTextField, with: defaultTopString)
+               configureTextField(bottomTextField, with: defaultBottomString)
+    }
+    
+    private func hideToolbar(hide: Bool) {
+        navigationController?.toolbar.isHidden = hide
+        toolbar.isHidden = hide
+    }
+    
+    private func takeScreenshot() -> UIImage? {
+        if selectedImage == nil {
+            return nil
+        }
+        
+        hideToolbar(hide: true)
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        hideToolbar(hide: false)
+        
+        return image
+    }
+    
+    private func configureTextField(_ textField: UITextField, with text: String) {
+        
+        let paragarghStyle = NSMutableParagraphStyle()
+        paragarghStyle.alignment = .center
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.paragraphStyle:  paragarghStyle,
+            NSAttributedString.Key.strokeWidth: -2
+        ]
+        
+        textField.defaultTextAttributes = attributes
+        textField.text = text
+        textField.delegate = self
+    }
+    
+    private func showImagePicker() {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWIllShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWIllShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardSize = keyboardFrame.cgRectValue
+            view.transform = .init(translationX: 0, y: -keyboardSize.height)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.transform = .identity
+    }
 }
 
+
+// MARK: Extenstions
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        
+        selectedImage = image
+        imageView.image = selectedImage
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+}
